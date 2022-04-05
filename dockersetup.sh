99ae4ff346aac0ee4cf1470a0ac74b9e1ef4def1
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2022-04-05 06:54:02 +0000

    Update dockersetup.sh

--- dockersetup.sh
+++ dockersetup.sh
@@ -2,14 +2,14 @@ CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
         touch $CONTAINER_ALREADY_STARTED
         echo "-- First container startup --"
-        echo " {\n"\
-         "  \"gateway_conf\": {\n" \
-         "      \"gateway_ID\": \"${gateway_ID}\",\n" \
-         "      \"server_address\": \"${server_address}\",\n" \
-         "      \"serv_port_up\": ${serv_port_up},\n" \
-         "      \"serv_port_down\": ${serv_port_down}\n" \
-         "    }\n"\
-         "  }\n" > /home/middleman/configs/config.json
+        echo " {"\
+         "  \"gateway_conf\": {" \
+         "      \"gateway_ID\": \"${gateway_ID}\"," \
+         "      \"server_address\": \"${server_address}\"," \
+         "      \"serv_port_up\": ${serv_port_up}," \
+         "      \"serv_port_down\": ${serv_port_down}" \
+         "    }"\
+         "  }" > /home/middleman/configs/config.json
 
         echo "middleman_ENVs=\"${middleman_ENVs}\"" > /home/middleman/middleman.conf
         cat /home/middleman/configs/config.json && cat /home/middleman/middleman.conf
